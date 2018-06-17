ff941c80447fef26a6e80d90312f9fb882c63d8a
Eidon <Eidon@tutanota.com>
2018-06-17 10:09:03 +0200

    Add files via upload
    New version of permusic.sh (checks that the size of the string is okay)

--- permusic.sh
+++ permusic.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
+
+timidity=/c/timidity/timidity 
+
 if [[ "$#" -ne 1 ]] ; then
-   echo "Please insert a string"
+   echo "Please insert a string whose number of characters is a multiple of 3"
    read perm
 else
    perm=$1
 fi
 
-echo $perm
+echo Processing permutations of $perm
+l=${#perm}
+
+if [ $(( l % 3 )) -ne 0 ] ; then
+    echo "Your string has a number of characters that is not a multiple of 3"
+	exit 1
+fi
 
 perm $perm > $perm.dat 
 data2lily -i $perm.dat -o $perm.ly -m glockenspiel -m harp -m celesta
 
 lilypond $perm.ly
 
-/c/timidity/timidity $perm.mid -O w -o $perm.wav
+$timidity $perm.mid -O w -o $perm.wav
 
 lame --abr 160k $perm.wav $perm.mp3
