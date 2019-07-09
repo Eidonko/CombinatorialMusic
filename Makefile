c194b040f37fa3374252590f858243c993f3d39d
Eidon <Eidon@tutanota.com>
2019-07-09 19:05:14 +0200

    Add files via upload
    Adjusted the Makefile so that the math library is specified when compiling the code.
    Removed bug in permusic.sh (error code of perm is 1 and not 0 when all is ok)

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 all:	data2lily perm
 
 data2lily:	data2lily3new.c
-		gcc -O3 -o data2lily data2lily3new.c
+		gcc -O3 -o data2lily data2lily3new.c -lm
 
 perm:	perm.w
 	ctangle perm.w
-	gcc -O3 -DPRINT -DTRI -o perm perm.c
+	gcc -O3 -DPRINT -DTRI -o perm perm.c -lm
 
 run:
 	permusic.sh
--- permusic.sh
+++ permusic.sh
@@ -1,50 +1,53 @@
 #!/usr/bin/env bash
 
-timidity=/c/timidity/timidity 
+#timidity=/c/timidity/timidity 
 
 if [[ "$#" -ne 1 ]] ; then
    echo "Please insert a string whose number of characters is a multiple of 3"
-   read perm
+   read permstring
 else
-   perm=$1
+   permstring=$1
 fi
 
-l=${#perm}
+l=${#permstring}
 
 if [ $(( l % 3 )) -ne 0 ] ; then
     echo "Your string has a number of characters that is not a multiple of 3"
 	exit 1
 fi
 
-echo Processing permutations of $perm
+echo Processing permutations of $permstring
 
-perm $perm > $perm.dat 
+echo "perm $permstring > $permstring.dat" 
+perm $permstring > $permstring.dat 
 
-if [[ "$?" -ne 0 ]] ; then
-    echo "perm $perm failed ($?) -- aborting execution"
+res="$?"
+
+if [ "$res" != "1" ] ; then
+    echo "perm $permstring failed ($res) -- aborting execution"
 	exit 2
 fi
 
 # add "-l" to avoid printing the score
-data2lily -i $perm.dat -o $perm.ly -m celesta -m glockenspiel -m kalimba
+data2lily -i $permstring.dat -o $permstring.ly -m celesta -m glockenspiel -m kalimba
 
-if [ $? -ne 0 ]; then
-    echo "data2lily -i $perm failed -- aborting execution"
+if [ "$?" -ne "0" ]; then
+    echo "data2lily -i $permstring failed -- aborting execution"
 	exit 3
 fi
 
-lilypond $perm.ly
+lilypond $permstring.ly
 
-if [ $? -ne 0 ]; then
-    echo "lilypond $perm.ly failed -- aborting execution"
+if [ "$?" -ne "0" ]; then
+    echo "lilypond $permstring.ly failed -- aborting execution"
 	exit 4
 fi
 
-$timidity $perm.mid -O w -o $perm.wav
+timidity $permstring.midi -O w -o $permstring.wav
 
-if [ $? -ne 0 ]; then
-    echo "timidity $perm.mid failed -- aborting execution"
+if [ "$?" -ne "0" ]; then
+    echo "timidity $permstring.mid failed -- aborting execution"
 	exit 5
 fi
 
-lame --quiet --abr 160k $perm.wav $perm.mp3 && rm -f $perm.wav
+lame --quiet --abr 160k $permstring.wav $permstring.mp3 && rm -f $permstring.wav
